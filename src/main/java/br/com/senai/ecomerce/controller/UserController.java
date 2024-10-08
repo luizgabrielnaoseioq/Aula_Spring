7baf14faeb76d6255b8d5643ad8768903ac6dad8
luiz_go_nazario <luiz_go_nazario@sesisenai.estudante.org.br>
2024-10-08 19:39:05 -0300

    Injetando uma dependência

--- src/main/java/br/com/senai/ecomerce/controller/UserController.java
+++ src/main/java/br/com/senai/ecomerce/controller/UserController.java
@@ -1,7 +1,14 @@
 package br.com.senai.ecomerce.controller;
 
+import br.com.senai.ecomerce.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController //supora apenas dados
+@RequestMapping("/user") // localhost:8080/user
 public class UserController {
+    // injeção de dependência, criar um objeto
+    @Autowired
+    private UserRepository userRepository;
 }
