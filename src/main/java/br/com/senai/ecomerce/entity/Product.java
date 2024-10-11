5f2a3c023946b20ce7c391c92d03c106c4368b1e
luiz_go_nazario <luiz_go_nazario@sesisenai.estudante.org.br>
2024-10-11 20:47:24 -0300

    Nova entidade criada chamada "Product"

---
+++ src/main/java/br/com/senai/ecomerce/entity/Product.java
@@ -0,0 +1,22 @@
+package br.com.senai.ecomerce.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private Float price;
+    private String brand;
+    private String color;
+}
